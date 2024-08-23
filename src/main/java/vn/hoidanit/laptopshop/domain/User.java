2726aa71cb93e279c3975c0cc3fb8dd3773e729a
Toan Duong <dktoan03@gmail.com>
2024-08-23 17:55:34 +0700

    update model

--- src/main/java/vn/hoidanit/laptopshop/domain/User.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/User.java
@@ -4,17 +4,32 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "users")
+
 public class User {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
+
   private String email;
+
   private String password;
   private String fullName;
   private String address;
   private String phone;
+
+  private String avatar;
+
+  @ManyToOne
+  @JoinColumn(name = "role_id")
+  private Role role;
+
+  // roleId
 
   public long getId() {
     return id;
@@ -64,10 +79,18 @@ public class User {
     this.phone = phone;
   }
 
+  public String getAvatar() {
+    return avatar;
+  }
+
+  public void setAvatar(String avatar) {
+    this.avatar = avatar;
+  }
+
   @Override
   public String toString() {
     return "User [id=" + id + ", email=" + email + ", password=" + password + ", fullName=" + fullName + ", address="
-        + address + ", phone=" + phone + "]";
+        + address + ", phone=" + phone + ", avatar=" + avatar + "]";
   }
 
 }
