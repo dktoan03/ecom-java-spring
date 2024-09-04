d9b4b026149b592cbcaf1ab3dbb2815f488cc87a
Toan Duong <dktoan03@gmail.com>
2024-09-05 00:44:34 +0700

    validate email and password final

--- src/main/java/vn/hoidanit/laptopshop/controller/client/HomePageController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/client/HomePageController.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import jakarta.validation.Valid;
 import vn.hoidanit.laptopshop.domain.Product;
 import vn.hoidanit.laptopshop.domain.User;
 import vn.hoidanit.laptopshop.domain.dto.RegisterDTO;
@@ -41,7 +43,12 @@ public class HomePageController {
   }
 
   @PostMapping("register")
-  public String handlePostRegister(@ModelAttribute("userRegister") RegisterDTO register) {
+  public String handlePostRegister(@ModelAttribute("registerUser") @Valid RegisterDTO register,
+      BindingResult bindingResult) {
+
+    if (bindingResult.hasErrors())
+      return "client/auth/register";
+
     User user = userService.RegisterDTOtoUser(register);
 
     String hashedPassword = this.passwordEncoder.encode(user.getPassword());
--- src/main/java/vn/hoidanit/laptopshop/service/UserService.java
+++ src/main/java/vn/hoidanit/laptopshop/service/UserService.java
@@ -53,4 +53,8 @@ public class UserService {
         user.setPassword(registerDTO.getPassword());
         return user;
     }
+
+    public Boolean checkEmailExist(String email) {
+        return this.userRepository.existsByEmail(email);
+    }
 }
