9511ee3da49d2ce72136ef35c765c9db84cd7f07
Toan Duong <dktoan03@gmail.com>
2024-09-10 16:05:56 +0700

    redirect after login

--- src/main/java/vn/hoidanit/laptopshop/config/SecurityConfiguration.java
+++ src/main/java/vn/hoidanit/laptopshop/config/SecurityConfiguration.java
@@ -3,13 +3,18 @@ package vn.hoidanit.laptopshop.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
+import jakarta.servlet.DispatcherType;
 import vn.hoidanit.laptopshop.service.CustomUserDetailsService;
 import vn.hoidanit.laptopshop.service.UserService;
 
@@ -26,16 +31,52 @@ public class SecurityConfiguration {
     return new CustomUserDetailsService(userService);
   }
 
+  // @Bean
+  // public AuthenticationManager authenticationManager(HttpSecurity http,
+  // PasswordEncoder passwordEncoder,
+  // UserDetailsService userDetailsService) throws Exception {
+  // AuthenticationManagerBuilder authenticationManagerBuilder = http
+  // .getSharedObject(AuthenticationManagerBuilder.class);
+  // authenticationManagerBuilder
+  // .userDetailsService(userDetailsService)
+  // .passwordEncoder(passwordEncoder);
+  // return authenticationManagerBuilder.build();
+  // }
   @Bean
-  public AuthenticationManager authenticationManager(HttpSecurity http,
+  public DaoAuthenticationProvider authProvider(
       PasswordEncoder passwordEncoder,
-      UserDetailsService userDetailsService) throws Exception {
-    AuthenticationManagerBuilder authenticationManagerBuilder = http
-        .getSharedObject(AuthenticationManagerBuilder.class);
-    authenticationManagerBuilder
-        .userDetailsService(userDetailsService)
-        .passwordEncoder(passwordEncoder);
-    return authenticationManagerBuilder.build();
+      UserDetailsService userDetailsService) {
+    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+    authProvider.setUserDetailsService(userDetailsService);
+    authProvider.setPasswordEncoder(passwordEncoder);
+    // authProvider.setHideUserNotFoundExceptions(false);
+    return authProvider;
+  }
+
+  @Bean
+  public AuthenticationSuccessHandler customSuccessHandler() {
+    return new CustomSuccessHandler();
+  }
+
+  @Bean
+  SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+    http
+        .authorizeHttpRequests(authorize -> authorize
+            .dispatcherTypeMatchers(DispatcherType.FORWARD,
+                DispatcherType.INCLUDE)
+            .permitAll()
+            .requestMatchers("/", "/login", "/product/**",
+                "/client/**", "/css/**", "/js/**", "/images/**")
+            .permitAll()
+            .requestMatchers("/admin/**").hasRole("ADMIN")
+            .anyRequest().authenticated())
+        .formLogin(formLogin -> formLogin
+            .loginPage("/login")
+            .failureUrl("/login?error")
+            .successHandler(customSuccessHandler())
+            .permitAll());
+    return http.build();
   }
 
 }
