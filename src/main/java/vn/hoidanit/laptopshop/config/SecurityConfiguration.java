0750669bf34f2455fa42437daeb797b983c5f5d4
Toan Duong <dktoan03@gmail.com>
2024-09-11 23:10:10 +0700

    remember me & keep session

--- src/main/java/vn/hoidanit/laptopshop/config/SecurityConfiguration.java
+++ src/main/java/vn/hoidanit/laptopshop/config/SecurityConfiguration.java
@@ -7,12 +7,14 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.session.security.web.authentication.SpringSessionRememberMeServices;
 
 import jakarta.servlet.DispatcherType;
 import vn.hoidanit.laptopshop.service.CustomUserDetailsService;
@@ -59,6 +61,14 @@ public class SecurityConfiguration {
   }
 
   @Bean
+  public SpringSessionRememberMeServices rememberMeServices() {
+    SpringSessionRememberMeServices rememberMeServices = new SpringSessionRememberMeServices();
+    // optionally customize
+    rememberMeServices.setAlwaysRemember(true);
+    return rememberMeServices;
+  }
+
+  @Bean
   SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
     http
@@ -71,6 +81,18 @@ public class SecurityConfiguration {
             .permitAll()
             .requestMatchers("/admin/**").hasRole("ADMIN")
             .anyRequest().authenticated())
+
+        .sessionManagement((sessionManagement) -> sessionManagement
+            .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
+            .invalidSessionUrl("/logout?expired")
+            .maximumSessions(1)
+            .maxSessionsPreventsLogin(false))
+
+        .logout(logout -> logout.deleteCookies("JSESSIONID").invalidateHttpSession(true))
+
+        .rememberMe((rememberMe) -> rememberMe
+            .rememberMeServices(rememberMeServices()))
+
         .formLogin(formLogin -> formLogin
             .loginPage("/login")
             .failureUrl("/login?error")
